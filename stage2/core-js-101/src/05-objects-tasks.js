85e1e34af22707d9a01707d429fa0452635509e9
Valentin Ak <41901260+OutLaw0@users.noreply.github.com>
2022-09-09 22:51:34 +0300

    Add files via upload

---
+++ stage2/core-js-101/src/05-objects-tasks.js
@@ -0,0 +1,193 @@
+/* ************************************************************************************************
+ *                                                                                                *
+ * Please read the following tutorial before implementing tasks:                                   *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object        *
+ *                                                                                                *
+ ************************************************************************************************ */
+
+
+/**
+ * Returns the rectangle object with width and height parameters and getArea() method
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {Object}
+ *
+ * @example
+ *    const r = new Rectangle(10,20);
+ *    console.log(r.width);       // => 10
+ *    console.log(r.height);      // => 20
+ *    console.log(r.getArea());   // => 200
+ */
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() { return width * height; },
+  };
+}
+
+
+/**
+ * Returns the JSON representation of specified object
+ *
+ * @param {object} obj
+ * @return {string}
+ *
+ * @example
+ *    [1,2,3]   =>  '[1,2,3]'
+ *    { width: 10, height : 20 } => '{"height":10,"width":20}'
+ */
+function getJSON(obj) {
+  return JSON.stringify(obj);
+}
+
+
+/**
+ * Returns the object of specified type from JSON representation
+ *
+ * @param {Object} proto
+ * @param {string} json
+ * @return {object}
+ *
+ * @example
+ *    const r = fromJSON(Circle.prototype, '{"radius":10}');
+ *
+ */
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const values = Object.values(obj);
+
+  return new proto.constructor(...values);
+}
+
+
+/**
+ * Css selectors builder
+ *
+ * Each complex selector can consists of type, id, class, attribute, pseudo-class
+ * and pseudo-element selectors:
+ *
+ *    element#id.class[attr]:pseudoClass::pseudoElement
+ *              \----/\----/\----------/
+ *              Can be several occurrences
+ *
+ * All types of selectors can be combined using the combination ' ','+','~','>' .
+ *
+ * The task is to design a single class, independent classes or classes hierarchy
+ * and implement the functionality to build the css selectors using the provided cssSelectorBuilder.
+ * Each selector should have the stringify() method to output the string representation
+ * according to css specification.
+ *
+ * Provided cssSelectorBuilder should be used as facade only to create your own classes,
+ * for example the first method of cssSelectorBuilder can be like this:
+ *   element: function(value) {
+ *       return new MySuperBaseElementSelector(...)...
+ *   },
+ *
+ * The design of class(es) is totally up to you, but try to make it as simple,
+ * clear and readable as possible.
+ *
+ * @example
+ *
+ *  const builder = cssSelectorBuilder;
+ *
+ *  builder.id('main').class('container').class('editable').stringify()
+ *    => '#main.container.editable'
+ *
+ *  builder.element('a').attr('href$=".png"').pseudoClass('focus').stringify()
+ *    => 'a[href$=".png"]:focus'
+ *
+ *  builder.combine(
+ *      builder.element('div').id('main').class('container').class('draggable'),
+ *      '+',
+ *      builder.combine(
+ *          builder.element('table').id('data'),
+ *          '~',
+ *           builder.combine(
+ *               builder.element('tr').pseudoClass('nth-of-type(even)'),
+ *               ' ',
+ *               builder.element('td').pseudoClass('nth-of-type(even)')
+ *           )
+ *      )
+ *  ).stringify()
+ *    => 'div#main.container.draggable + table#data ~ tr:nth-of-type(even)   td:nth-of-type(even)'
+ *
+ *  For more examples see unit tests.
+ */
+
+const cssSelectorBuilder = {
+  selector: '',
+
+  element(value) {
+    this.checkErrors(1);
+    const object = Object.create(cssSelectorBuilder);
+    object.error = 1;
+    object.selector = this.selector + value;
+    return object;
+  },
+
+  id(value) {
+    this.checkErrors(2);
+    const object = Object.create(cssSelectorBuilder);
+    object.error = 2;
+    object.selector = `${this.selector}#${value}`;
+    return object;
+  },
+
+  class(value) {
+    this.checkErrors(3);
+    const object = Object.create(cssSelectorBuilder);
+    object.error = 3;
+    object.selector = `${this.selector}.${value}`;
+    return object;
+  },
+
+  attr(value) {
+    this.checkErrors(4);
+    const object = Object.create(cssSelectorBuilder);
+    object.error = 4;
+    object.selector = `${this.selector}[${value}]`;
+    return object;
+  },
+
+  pseudoClass(value) {
+    this.checkErrors(5);
+    const object = Object.create(cssSelectorBuilder);
+    object.error = 5;
+    object.selector = `${this.selector}:${value}`;
+    return object;
+  },
+
+  pseudoElement(value) {
+    this.checkErrors(6);
+    const object = Object.create(cssSelectorBuilder);
+    object.error = 6;
+    object.selector = `${this.selector}::${value}`;
+    return object;
+  },
+
+  combine(selector1, combinator, selector2) {
+    const object = Object.create(cssSelectorBuilder);
+    object.selector = `${selector1.selector} ${combinator} ${selector2.selector}`;
+    return object;
+  },
+
+  stringify() {
+    return this.selector;
+  },
+
+  checkErrors(error) {
+    if (error < this.error) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    if ((error === 1 || error === 2 || error === 6) && this.error === error) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+  },
+};
+
+
+module.exports = {
+  Rectangle,
+  getJSON,
+  fromJSON,
+  cssSelectorBuilder,
+};
